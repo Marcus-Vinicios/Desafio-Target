8ee628f10ec6e34ad0ab1233ec14619c6998225a
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2023-05-03 07:47:23 -0300

    desafio-02_Target

--- Desafio-02.js
+++ Desafio-02.js
@@ -1,13 +1,14 @@
 console.log('Desafio-02')
-const v1 = 34
-const i = [0, 1]
+const v1 = 21 //entrada de valor;
+const i = [0, 1] //array de armazenamento dos valores;
 
 while (i[i.length - 1] < v1) {
-    const v2 = i[i.length - 1] + i[i.length - 2]
-    i.push(v2)
+    const v2 = i[i.length - 1] + i[i.length - 2] //soma dos valores do array;
+    i.push(v2) //inserção de valores no array;
     console.log(v2)
 }
 
+//verifica se o numero pertence ou não à sequencia de Fibonacci
 if (i[i.length - 1] === v1) {
     console.log(`O número ${v1} pertence à sequência de Fibonacci.`)
 } else {
